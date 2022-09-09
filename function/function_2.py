f451ddf3a9305d58a3a460f51e4d6b5f15029da9
Riponbarua <110828438+Riponbarua@users.noreply.github.com>
2022-09-09 18:27:03 +0800

    Add files via upload

---
+++ function/function_2.py
@@ -0,0 +1,46 @@
+#return value
+def add_two_values(firt,second):
+    return firt + second  # Addition
+number_1 = 10
+number_2 = 13
+
+print (f"{number_1} + {number_2} = {number_1 + number_2}") # number_1 + number_2 is parameter
+#or
+sum = add_two_values(number_1,number_2)
+print(sum)
+
+# subtraction
+
+def add_two_values(firt,second):
+    return firt - second
+number_1 = 10
+number_2 = 13
+
+print (f"{number_1} + {number_2} = {number_1 + number_2}") # number_1 + number_2 is parameter
+#or
+sum = add_two_values(number_1,number_2)
+
+
+print(sum)
+# multiplication
+def add_two_values(firt,second):
+    return firt * second
+number_1 = 10
+number_2 = 13
+
+print (f"{number_1} + {number_2} = {number_1 + number_2}") # number_1 + number_2 is parameter
+#or
+sum = add_two_values(number_1,number_2)
+print(sum)
+
+# division
+
+def add_two_values(firt,second):
+    return firt / second
+number_1 = 10
+number_2 = 13
+
+print (f"{number_1} + {number_2} = {number_1 + number_2}") # number_1 + number_2 is parameter
+#or
+sum = add_two_values(number_1,number_2)
+print(sum)
