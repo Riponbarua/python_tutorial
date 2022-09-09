d87dfcc472635d31ea0c0709968662d00e25e482
Riponbarua <110828438+Riponbarua@users.noreply.github.com>
2022-09-09 18:26:37 +0800

    Add files via upload

---
+++ input/input-1.py
@@ -0,0 +1,5 @@
+user_name=input("your name : ") #need a variable // here user_name is a variable
+print("hello "+user_name)
+
+your_name=input("what is your name ?")
+print("my name is " +your_name)
